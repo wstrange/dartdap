50c4bf84b645bd9194b74f0efd4d08c6eb6674ff
Chris Ridd <chrisridd@mac.com>
2015-02-28 21:40:02 +0000

    Test encoding various filters
    Currently it looks like all "!" filters zero out the last 2 octets,
    so a number of tests fail.

--- test/filter_test.dart
+++ test/filter_test.dart
@@ -1,61 +1,84 @@
-/// Unit test for filter encodings
-
-
-/*
- * Obj C test supplied by Chris.  TODO: Convert this to a Dart unit test
- *
- * [eq writeFilter:@"(foo=bar)"];
-    uint8_t eq_bytes[] = { 0xa3, 0x0a, 0x04, 0x03, 'f', 'o', 'o', 0x04, 0x03, 'b', 'a', 'r' };
-
-    [le writeFilter:@"(foo<=bar)"];
-    uint8_t lt_bytes[] = { 0xa6, 0x0a, 0x04, 0x03, 'f', 'o', 'o', 0x04, 0x03, 'b', 'a', 'r' };
-
-    [ge writeFilter:@"(foo>=bar)"];
-    uint8_t gt_bytes[] = { 0xa5, 0x0a, 0x04, 0x03, 'f', 'o', 'o', 0x04, 0x03, 'b', 'a', 'r' };
-
-    [ap writeFilter:@"(foo~=bar)"];
-    uint8_t ap_bytes[] = { 0xa8, 0x0a, 0x04, 0x03, 'f', 'o', 'o', 0x04, 0x03, 'b', 'a', 'r' };
-
-    [ls writeFilter:@"(foo=bar*)"];
-    uint8_t ls_bytes[] = { 0xa4, 0x0c, 0x04, 0x03, 'f', 'o', 'o', 0x30, 0x05, 0x80, 0x03, 'b', 'a', 'r' };
-
-    [pr writeFilter:@"(foo=*)"];
-    uint8_t pr_bytes[] = { 0x87, 0x03, 'f', 'o', 'o' };
-
-    [ne writeFilter:@"(!(foo=bar))"];
-    uint8_t ne_bytes[] = { 0xa2, 0x0c, 0xa3, 0x0a, 0x04, 0x03, 'f', 'o', 'o', 0x04, 0x03, 'b', 'a', 'r' };
-
-    [nl writeFilter:@"(!(foo<=bar))"];
-    uint8_t nl_bytes[] = { 0xa2, 0x0c, 0xa6, 0x0a, 0x04, 0x03, 'f', 'o', 'o', 0x04, 0x03, 'b', 'a', 'r' };
-
-    [ng writeFilter:@"(!(foo>=bar))"];
-    uint8_t ng_bytes[] = { 0xa2, 0x0c, 0xa5, 0x0a, 0x04, 0x03, 'f', 'o', 'o', 0x04, 0x03, 'b', 'a', 'r' };
-
-    [na writeFilter:@"(!(foo~=bar))"];
-    uint8_t na_bytes[] = { 0xa2, 0x0c, 0xa8, 0x0a, 0x04, 0x03, 'f', 'o', 'o', 0x04, 0x03, 'b', 'a', 'r' };
-
-    [np writeFilter:@"(!(foo=*))"];
-    uint8_t np_bytes[] = { 0xa2, 0x05, 0x87, 0x03, 'f', 'o', 'o' };
-
-    [and writeFilter:@"(&(foo=bar)(baz=banana))"];
-    uint8_t and_bytes[] = { 0xa0, 0x1b,
-        0xa3, 0x0a, 0x04, 0x03, 'f', 'o', 'o', 0x04, 0x03, 'b', 'a', 'r',
-        0xa3, 0x0d, 0x04, 0x03, 'b', 'a', 'z', 0x04, 0x06, 'b', 'a', 'n', 'a', 'n', 'a' };
-        *
- */
-
+/// Unit tests for filter encodings
 
 
 import 'package:unittest/unittest.dart';
 import 'package:dartdap/dartdap.dart';
-import 'package:logging_handlers/logging_handlers_shared.dart';
 
 main() {
 
+  group('Filter Encoding', () {
 
-  test('Filter Encoding', () {
-    // put test here.
+    test('(foo=bar)', () {
+      var f = Filter.equals("foo", "bar");
+      var b = f.toASN1().encodedBytes;
+      expect(b, equals([0xa3, 0x0a, 0x04, 0x03, 0x66, 0x6f, 0x6f, 0x04, 0x03, 0x62, 0x61, 0x72]));
+    });
+
+    test('(foo<=bar)', () {
+      var f = new Filter(Filter.TYPE_LESS_OR_EQUAL, "foo", "bar");
+      var b = f.toASN1().encodedBytes;
+      expect(b, equals([0xa6, 0x0a, 0x04, 0x03, 0x66, 0x6f, 0x6f, 0x04, 0x03, 0x62, 0x61, 0x72]));
+    });
+
+    test('(foo>=bar)', () {
+      var f = new Filter(Filter.TYPE_GREATER_OR_EQUAL, "foo", "bar");
+      var b = f.toASN1().encodedBytes;
+      expect(b, equals([0xa5, 0x0a, 0x04, 0x03, 0x66, 0x6f, 0x6f, 0x04, 0x03, 0x62, 0x61, 0x72]));
+    });
+
+    test('(foo~=bar)', () {
+      var f = new Filter(Filter.TYPE_APPROXIMATE_MATCH, "foo", "bar");
+      var b = f.toASN1().encodedBytes;
+      expect(b, equals([0xa8, 0x0a, 0x04, 0x03, 0x66, 0x6f, 0x6f, 0x04, 0x03, 0x62, 0x61, 0x72]));
+    });
+
+    test('(foo=bar*)', () {
+      var f = Filter.substring("foo=bar*");
+      var b = f.toASN1().encodedBytes;
+      expect(b, equals([0xa4, 0x0c, 0x04, 0x03, 0x66, 0x6f, 0x6f, 0x30, 0x05, 0x80, 0x03, 0x62, 0x61, 0x72]));
+    });
+
+    test('(foo=*)', () {
+      var f = Filter.present("foo");
+      var b = f.toASN1().encodedBytes;
+      expect(b, equals([0x87, 0x03, 0x66, 0x6f, 0x6f]));
+    });
+
+    test('(!(foo=bar))', () {
+      var f = Filter.not(Filter.equals("foo", "bar"));
+      var b = f.toASN1().encodedBytes;
+      expect(b, equals([0xa2, 0x0c, 0xa3, 0x0a, 0x04, 0x03, 0x66, 0x6f, 0x6f, 0x04, 0x03, 0x62, 0x61, 0x72]));
+    });
+
+    test('(!(foo<=bar))', () {
+      var f = Filter.not(new Filter(Filter.TYPE_LESS_OR_EQUAL, "foo", "bar"));
+      var b = f.toASN1().encodedBytes;
+      expect(b, equals([0xa2, 0x0c, 0xa6, 0x0a, 0x04, 0x03, 0x66, 0x6f, 0x6f, 0x04, 0x03, 0x62, 0x61, 0x72]));
+    });
+
+    test('(!(foo>=bar))', () {
+      var f = Filter.not(new Filter(Filter.TYPE_GREATER_OR_EQUAL, "foo", "bar"));
+      var b = f.toASN1().encodedBytes;
+      expect(b, equals([0xa2, 0x0c, 0xa5, 0x0a, 0x04, 0x03, 0x66, 0x6f, 0x6f, 0x04, 0x03, 0x62, 0x61, 0x72]));
+    });
+
+    test('(!(foo~=bar))', () {
+      var f = Filter.not(new Filter(Filter.TYPE_APPROXIMATE_MATCH, "foo", "bar"));
+      var b = f.toASN1().encodedBytes;
+      expect(b, equals([0xa2, 0x0c, 0xa8, 0x0a, 0x04, 0x03, 0x66, 0x6f, 0x6f, 0x04, 0x03, 0x62, 0x61, 0x72]));
+    });
+
+    test('(!(foo=*))', () {
+      var f = Filter.not(Filter.present("foo"));
+      var b = f.toASN1().encodedBytes;
+      expect(b, equals([0xa2, 0x05, 0x87, 0x03, 0x66, 0x6f, 0x6f]));
+    });
+
+    test('(&(foo=bar)(baz=banana))', () {
+      var f = Filter.and([Filter.equals("foo", "bar"), Filter.equals("baz", "banana")]);
+      var b = f.toASN1().encodedBytes;
+      expect(b, equals([0xa0, 0x1b, 0xa3, 0x0a, 0x04, 0x03, 0x66, 0x6f, 0x6f, 0x04, 0x03, 0x62, 0x61, 0x72, 0xa3, 0x0d, 0x04, 0x03, 0x62, 0x61, 0x7a, 0x04, 0x06, 0x62, 0x61, 0x6e, 0x61, 0x6e, 0x61]));
+    });
 
   });
 }
-
