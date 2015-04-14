1b1868abbd9e9841bf44640acd1e288108acd997
Warren Strange <warren.strange@gmail.com>
2015-04-14 14:25:42 -0600

    Updated message transformer

--- lib/src/connection/ldap_transformer.dart
+++ lib/src/connection/ldap_transformer.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'dart:typed_data';
 import '../protocol/ldap_protocol.dart';
+import 'package:asn1lib/asn1lib.dart';
 
 /// Returns a [StreamTransformer<Uint8List,LDAPMessage>] that transform a stream
 /// of bytes to a stream of LDAP messages.
@@ -15,12 +16,12 @@ StreamTransformer<Uint8List, LDAPMessage> createTransformer() {
 
     // Set buf to the bytes to attempt to process: leftover bytes from an
     // earlier data event (if any) plus the new bytes in data.
-
     var buf;
     if (leftover == null) {
       // No left over bytes from before: new data only
       buf = new Uint8List.view(data.buffer);
     } else {
+      logger.finest("Merging ${leftover.length}  bytes ");
       // There were left over bytes: leftover bytes + new data
       buf = new Uint8List(leftover.length + data.length);
       buf.setRange(0, leftover.length, leftover);
@@ -34,71 +35,63 @@ StreamTransformer<Uint8List, LDAPMessage> createTransformer() {
 
     assert(0 < buf.length);
 
+    int count = 0; // count of total bytes consumed
+
     do {
-      // Try to determine the length of the next ASN1 object
-
-      var value_size = null; // null if insufficient bytes to determine length
-      var length_size; // number of bytes used by length field
-
-      if (2 <= buf.length) {
-        // Enough data for the start of the length field
-        int first_length_byte = buf[1]; // note: tag is at position 0
-        if (first_length_byte & 0x80 == 0) {
-          // Single byte length field
-          length_size = 1;
-          value_size = first_length_byte & 0x7F;
-        } else {
-          // Multi-byte length field
-          var num_length_bytes = (first_length_byte & 0x7F);
-          if (2 + num_length_bytes <= buf.length) {
-            // Enough data for the entire length field
-            length_size = 1 + num_length_bytes;
-            value_size = 0;
-            for (int x = 2; x < num_length_bytes + 2; x++) {
-              value_size <<= 8;
-              value_size |= buf[x] & 0xFF;
-            }
-          }
-        }
+      // only 1 byte? Can't possibly decode a length - wait for more bytes
+      if (buf.length < 2) {
+        leftover = buf;
+        logger.finest("need at least 2 bytes in stream. Waiting for more");
+        return;
+      }
+      // Try to determine the length of the ASN1 object so we
+      // know how many bytes to expect in the stream
+      // It is possible (but not likely) that this could throw a RangeError
+      // if the stream does not have enough bytes to decode the length
+      // This should be quite rare - so using an exception here is probably justified.
+      int message_length = null;
+      try {
+        var asn1length = ASN1Length.decodeLength(buf);
+        // asn1length computes the length of value bytes - so
+        // add the start position in to get total message length
+        message_length = asn1length.length + asn1length.valueStartPosition;
+      } on RangeError catch (e) {
+        logger.finest('Not enough bytes to decode length. Waiting for more');
+        leftover = buf;
+        return;
       }
 
-      if (value_size != null && (1 + length_size + value_size) <= buf.length) {
-        // Got length and there is sufficient data for the complete ASN1 object
-
-        var message_size = (1 + length_size + value_size); // tag, length, data
-
-        logger.fine(
-            "LDAP stream transformer: ${message_size} bytes for ASN1 object: tag=${buf[0]}, length=${value_size} value=${new Uint8List.view(buf.buffer, 1 + length_size, value_size)}");
-
-        if (buf[0] == 10) {
-          // TODO: debug why this tag is not being parsed properly
-          logger.finest("LDAP stream transformer: got a tag 10 object");
-        }
-
-        // Create LDAPMessage from all the bytes of the complete ASN1 object.
-
-        var msg = new LDAPMessage.fromBytes(buf);
-        assert(msg.messageLength == message_size);
-        logger.fine("Received LDAP message ${msg}");
-        sink.add(msg); // put on output stream
-
-        // Update buf: discard the message's bytes and keep any remaining
-        // bytes. If no bytes remain, set buf to null to exit the do-while processing loop.
-
-        if (buf.length == message_size) {
-          buf = null; // all bytes completely processed
-        } else {
-          buf = new Uint8List.view(buf.buffer, message_size);
-        }
-      } else {
-        // Insufficient data for a complete ASN1 object.
-
-        leftover = buf; // save bytes until more data arrives
-        buf = null; // so the do-while processing loop exits
-
+      // length was decoded OK - so now see
+      // If we have a full ldap message
+      if (buf.length < message_length) {
         logger.finest(
-            "LDAP stream transformer: incomplete ASN1 object: ${leftover.length} bytes retained (${leftover})");
+            "buffer does not have enough bytes for a full LDAP message. Waiting for more bytes");
+        leftover = buf;
+        return;
       }
+
+      try {
+        var msg = new LDAPMessage.fromBytes(buf);
+        assert(msg.messageLength == message_length);
+        logger.finest(
+            "Received LDAP message ${msg} size =${msg.messageLength}");
+        sink.add(msg); // put it on the output stream
+      } catch (e) {
+        logger.severe(
+            "Caught exception while trying to create ldap message, ${e.message}");
+        // todo: Do we attempt to carry on and recover, or rethrow?
+        throw (e);
+      }
+
+      // If no bytes remain we are done
+      if (buf.length == message_length) {
+        leftover = null; // and no extra bytes to process
+        return;
+      }
+
+      // advance buffer window to next message
+      count += message_length;
+      buf = new Uint8List.view(data.buffer, count);
     } while (buf != null);
   }, handleError: (Object error, StackTrace st, EventSink<LDAPMessage> sink) {
     logger.severe("LDAP stream transformer: error=${error}, stacktrace=${st}");
@@ -106,5 +99,3 @@ StreamTransformer<Uint8List, LDAPMessage> createTransformer() {
     throw error;
   });
 }
-
-//EOF
